d014ec2e688973072c30feeb9d82d9b4d8508a2d
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-09-07 09:02:40 -0700

    Added the 6-size.c file

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/*This is a c program */
+/**
+ *  main - This is a program that prints a statement using the puts() funtion
+ *
+ *  Return: Always success
+ */
+
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of a char: %d byte(s)\n", (int)sizeof(a));
+	printf("Size of a int: %d byte(s)\n", (int)sizeof(b));
+	printf("Size of a long int: %d byte(s)\n", (int)sizeof(c));
+	printf("Size of a long long int: %d byte(s)\n", (int)sizeof(d));
+	printf("Size of a float: %d byte(s)\n", (int)sizeof(e));
+	return (0);
+}
