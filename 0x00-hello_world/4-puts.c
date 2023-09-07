f3211d01c20052ccd3d2b010da6a3ed0dd02523a
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-09-07 08:13:23 -0700

    Edited the 4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+/**
+ * This is a program that prints a statement using the puts() funtion
+ * /
 
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
