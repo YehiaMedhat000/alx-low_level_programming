79ec0b86d8a7eb959cc7fdc3d823e93ba79f6d9e
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-09-07 08:36:48 -0700

    Added the 5-printf.c file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*This is a c program */
+/**
+ *  main - This is a program that prints a statement using the puts() funtion
+ *
+ *  Return: Always success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
