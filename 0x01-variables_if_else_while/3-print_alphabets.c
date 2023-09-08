21baaa3c84d2935de544cdbcc85fde90e3ff3f3e
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-09-08 16:35:31 +0300

    Added the 3-print_alphabets.c file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include <stdio.h>
+/**
+ * main - prints the alphabet lower case
+ *
+ * Return: 0 (sucess)
+ */
+
+int main(void)
+{
+	char str[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	printf("%s\n", str);
+	return (0);
+}
