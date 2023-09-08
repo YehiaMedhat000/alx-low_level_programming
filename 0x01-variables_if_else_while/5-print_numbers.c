9460a5827db23bd1348c6f3c1a4c1bf916354000
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-09-08 18:58:09 +0300

    Added the 5-print_numbers.c file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include <stdio.h>
+/**
+ * main - prints the numbers of base 10
+ *
+ * Return: 0 (sucess)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		printf("%d", i);
+	}
+	
+	printf("\n");
+	return (0);
+}
