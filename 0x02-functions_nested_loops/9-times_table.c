1cdfa5d974dd3f537f1af7733adc895d076fd311
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-09-13 01:29:28 +0300

    Edited the 9-time_tables.c file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,12 +9,15 @@ void times_table(void)
 	int i;
 	int j;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j < 9; j++)
+		for (j = 0; j <= 9; j++)
 		{
-			_putchar(' ');
-			_putchar(' ');
+			if (j != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+			}
 			_putchar(48 + (i * j));
 			if (j != 9)
 			{
