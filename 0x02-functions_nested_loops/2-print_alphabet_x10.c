fd7f3ef5d4635b2c193ccbf08f312d136c1fa9bf
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-09-12 19:47:04 +0300

    Added the 2-print_alphabet_x10.c file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - prints some word
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		print("abcdefghijklmnopqrstuvwxyz\n");
+	}
+	return (0);
+}
+
