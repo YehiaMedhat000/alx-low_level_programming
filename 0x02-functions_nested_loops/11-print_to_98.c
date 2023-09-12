7cbeffb0d27fba5c8f7c1efcf1c96d328a8258fe
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-09-13 02:21:52 +0300

    Added the last task file

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - prints all nat numbers from n to 98
+ * @n: param of type int
+ * Return: None, void
+ */
+
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
+}
