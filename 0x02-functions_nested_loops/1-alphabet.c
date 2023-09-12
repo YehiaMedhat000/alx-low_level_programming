4fd888286004a3638b61c76d2dced12e4480aa4b
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-09-12 15:13:22 +0300

    Edited the 1-alphabet.c file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,8 +4,15 @@
  * Return: Always 0 (success)
  */
 
+void print_alphabet(void);
+
 int main(void)
 {
-print("abcdefghijklmnopqrstuvwxyz\n");
+print_alphabet();
 return (0);
+}
+
+void print_alphabet()
+{
+print("abcdefghijklmnopqrstuvwxyz\n");
 }
