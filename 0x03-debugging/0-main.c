022d7a3b42f250c107d4b16f5b25211b24c634c4
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-09-13 22:17:25 +0300

    Edited the files

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,16 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
 #include "main.h"
+
 /**
- * main - entry point
- * Reuturn: According to the test
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
 int main(void)
 {
-	positive_or_negative(0);
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
 	return (0);
 }
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,13 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-
-/**
- * This is a header file
- */
 
 void positive_or_negative(int i);
 
-#endif
+#endif /* MAIN_H */
