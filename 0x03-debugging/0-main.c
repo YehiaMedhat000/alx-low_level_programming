ae7fd4b50ea4b0dee37b317bbf70d12bfecdfc36
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-09-13 19:53:58 +0300

    Added the 0-main.c and main.h files

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include "main.h"
+/**
+ * main - entry point
+ * Reuturn: According to the test
+ */
+int main(void)
+{
+	positive_or_negative(0);
+	return (0);
+}
