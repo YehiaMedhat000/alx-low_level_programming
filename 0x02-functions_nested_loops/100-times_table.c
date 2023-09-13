0a0cb5dc5913057477550be98dab48c866894823
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-09-13 14:57:15 +0300

    Edited the 100-times_table.c file

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -32,13 +32,19 @@ void print_times_table(int n)
 				_putchar(' ');
 			}
 			else if (p <= 99)
+			{
 				_putchar(' ');
 				_putchar(' ');
+			}
 			else if (p >= 100)
+			{
 				_putchar(48 + p / 100);
 				_putchar(48 + (p % 100) / 10);
+			}
 			else
+			{
 				_putchar(48 + p / 10);
+			}
 
 			_putchar(48 + p % 10);
 		}
