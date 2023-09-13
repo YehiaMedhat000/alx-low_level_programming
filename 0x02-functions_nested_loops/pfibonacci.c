6d4690fc17a29e4907f2a924c9f331ba0163363d
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-09-13 16:39:35 +0300

    Added the 102-fibonacci.c file

---
+++ 0x02-functions_nested_loops/pfibonacci.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * pfibonacci - prints n elements of fibonacci series
+ * @n: type int
+ * Return: None, void
+ */
+void pfibonacci(int n)
+{
+	int i = 0;
+	int f1, f2, sum = 0;
+	
+	if (i == n)
+	{
+		return;
+	}
+
+		printf("%d\n", sum);
+	pfibonacci();
+}
