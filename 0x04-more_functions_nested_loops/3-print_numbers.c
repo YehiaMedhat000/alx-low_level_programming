c70d81f0df01cc328def29ab2af518434195d64b
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-09-14 09:05:30 +0300

    Added the 3-print_numbers.c file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - prints 0-9 followed by \n
+ * Return: None, void
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		printf("%d", i);
+	}
+
+	printf("\n");
+}
