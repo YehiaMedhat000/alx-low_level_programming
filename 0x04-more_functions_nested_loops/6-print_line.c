1d51947097b953e148e80a0ebe2673163914d343
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-09-14 09:37:52 +0300

    Added the 6-print_line.c file

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_line - prints _ n times followed by \n
+ * if n is 0 or less it prints \n only
+ * @n: type int
+ *
+ * Return: None, void
+ */
+
+void print_line(int n)
+{
+	int i;
+	
+	if (n =< 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
