b2946d090a9fdc4f71cdc4d2b1ae3bd5d7c54053
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-09-14 09:12:11 +0300

    Added the 4-print_most_numbers.c file

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints 0-9 
+ * without 2,4 followed by \n
+ *
+ * Return: None, void
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48 ; i < 58 ; i++)
+	{
+		if (i == '2' || i == '4')
+		{
+			continue;
+		}
+		_putchar(i);
+	}
+
+	printf("\n");
+}
