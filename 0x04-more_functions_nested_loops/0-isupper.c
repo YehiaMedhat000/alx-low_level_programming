bb30d5f829553d7b69ec34d63814d854ce422577
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-09-14 08:54:18 +0300

    Added the header file and 0-isupper.c file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isupper - checks whether a char is uppercase
+ * @c: type char
+ * Return: if c isn't upper 0
+ * and 1 if c is upper
+ */
+
+int _isupper(char c)
+{
+	if (c >= 65 || c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
