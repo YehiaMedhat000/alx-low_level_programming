7474f73c540db6e0b453bd385cf56da229f6f67c
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-09-14 09:21:16 +0300

    Added the 5-more_numbers.c file

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * most_numbers - prints 0-14 ten lines followed by \n
+ * Return: None, void
+ */
+
+void most_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0 ; j < 15 ; j++)
+		{
+			_putchar(j % 10 + '0');
+		}
+	}
+
+	printf("\n");
+}
