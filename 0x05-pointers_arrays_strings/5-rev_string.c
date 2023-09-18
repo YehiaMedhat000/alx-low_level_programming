eb915b4bbc51f373375b52dde8be26eaf8860c37
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-09-18 18:12:09 +0300

    Edited the 5-rev_string.c file

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: pointer to the first element of the array
+ * Return: Nothing, void
+ */
+void rev_string(char *s)
+{
+	int i;
+	int len = strlen(s);
+	char t;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		t = *(s + i);
+		*(s + i) = *(s + len - 1 - i);
+		*(s + len - 1 - i) = t;
+		printf("t: %c, *(s + i): %c, *(s + len - 1 - i): %c\n", t, *(s + i), *(s + len - 1 - i));
+	}
+
+}
