8171d1828fd35fcdf3e3cdb593aa8228e031fdf0
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-09-18 14:02:25 +0300

    Edited the 4-print_rev.c file

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,17 +1,33 @@
 #include "main.h"
 
 /**
+ * _strlen - Gets the length of a string array
+ * @s: pointer to the first element of the array
+ * Return: the length of the string, type int
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; *s != '\0'; i++)
+	{
+		s++;
+	}
+
+	return (i);
+}
+
+/**
  * print_rev - prints a string in reverse
  * @s: pointer to the first element of the array
  * Return: Nothing, void
  */
-
 void print_rev(char *s)
 {
 	int i;
 	int len = _strlen(s);
 
-	for (i = len; i > 1; i--)
+	for (i = len; i >= 0; i--)
 	{
 		_putchar(*(s + i));
 	}
