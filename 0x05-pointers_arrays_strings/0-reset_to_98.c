d70041c32c8c05e2a06fa15a0ed1b2106ce48b32
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-09-18 12:11:59 +0300

    Edited the 0-reset_to_98.c file

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,10 +2,10 @@
 
 /**
  * reset_to_98 - changes an integer value to 98 by refrence
- * @*n: pointer of the integer to be changed
+ * @n: pointer of the integer to be changed
  * Return: Nothing, void
  */
 void reset_to_98(int *n)
 {
-        *n = 98;
+	*n = 98;
 }
