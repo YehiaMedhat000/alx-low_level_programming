160ed58df0a9cb346834a11d292b4f8b7b596b7b
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-09-18 12:08:36 +0300

    Added the 0-reset_to_98.c file, edited .gitignore

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - changes an integer value to 98 by refrence
+ * @*n: pointer of the integer to be changed
+ * Return: Nothing, void
+ */
+void reset_to_98(int *n)
+{
+        *n = 98;
+}
