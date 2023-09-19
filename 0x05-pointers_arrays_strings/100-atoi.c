aea0bea0a97b5a5786c3a49133d5ea87c43bcc66
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-09-19 10:21:43 +0300

    Added my solution for the last task

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,40 +1,55 @@
+#include <string.h>
 #include "main.h"
 
 /**
- * _atoi - Convert a string to an integer.
- * @s: The pointer to convert
- *
- * Return: A integer
+ * _atoi - extracts the digits from a given string
+ * @s: pointer to the first element of the array
+ * Return: integer in the string
  */
+
 int _atoi(char *s)
 {
-	int c = 0;
-	unsigned int ni = 0;
-	int min = 1;
-	int isi = 0;
+	int i, j, idx; 
+	int len = strlen(s);
+	int num_len = 0;
+	int sum = 0;
 
-	while (s[c])
+	for (idx = 0; idx < len; idx++)
 	{
-		if (s[c] == 45)
+		if (s[idx] >= 48 && s[idx] <= 57)
 		{
-			min *= -1;
+			num_len++;
+			if (!(s[idx + 1] >= 48 && s[idx + 1] <= 57))
+			{
+				break;
+			}
 		}
-
-		while (s[c] >= 48 && s[c] <= 57)
-		{
-			isi = 1;
-			ni = (ni * 10) + (s[c] - '0');
-			c++;
-		}
-
-		if (isi == 1)
-		{
-			break;
-		}
-
-		c++;
 	}
 
-	ni *= min;
-	return (ni);
+	for (i = idx - num_len + 1; i < idx + 1; i++)
+	{
+		if (s[i - 1] == 45 || sum < 0)
+		{
+			int mul = 1;
+
+			for (j = 1; j < num_len; j++)
+			{
+				mul *= 10;
+			}
+			sum -= mul * (s[i] - 48);
+		}
+
+		else
+		{
+			int mul = 1;
+
+			for (j = 1; j < num_len; j++)
+			{
+				mul *= 10;
+			}
+			sum += mul * (s[i] - 48);
+		}
+		num_len--;
+	}
+	return (sum);
 }
