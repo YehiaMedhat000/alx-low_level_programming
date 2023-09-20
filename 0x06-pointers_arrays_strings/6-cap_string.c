e602af52f08f72de71c240a9bfaa0e94ee1487b9
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-09-20 23:49:19 +0300

    Edited the 6-cap_string.c file

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,12 +10,22 @@
 char *cap_string(char *s)
 {
 	int i = 0;
+	int j;
+	char del[] = " ,;.!?\"(){}";
 
-	while (s[i] != '\0')
+	while (s[i])
 	{
 		if (s[i] >= 97 && s[i] <= 122)
 		{
-			s[i] -= 32;
+			j = 0;
+			while (del[j])
+			{
+				if (s[i - 1] == del[j] || i == 0)
+				{
+					s[i] -= 32;
+				}
+				j++;
+			}
 		}
 		i++;
 	}
