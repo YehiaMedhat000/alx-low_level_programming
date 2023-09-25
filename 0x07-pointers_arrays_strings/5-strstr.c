3f92c1d26849cec10ed4d143e35b8a918035f04d
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-09-25 22:47:51 +0300

    Edited the 5-strstr.c file

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,16 +9,6 @@
  * char in needle in haystack
  */
 
-/**
-* _strstr - locates a substring
-*
-* @haystack: the longer string to search
-* @needle: the substring to search for
-*
-* Return: a pointer to the beginning of the located substring, or NULL if
-* the substring is not found.
-*/
-
 char *_strstr(char *haystack, char *needle)
 {
 	int i;
