64ec9e5b5841f46c632abd791ce324e12c4fa966
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-09-25 23:58:38 +0300

    Added the 8-print_diagsums.c file

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagsums - Prints the sums of the diagonals 
+ * for a square matrix
+ * @a: Char pointer for the first subarray
+ * @size: size of the matrix
+ * Return: Nothing, void
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, sum_main = 0, sum_other = 0;
+
+	for (i = 0; i < size * size; i += size + 1)
+	{
+		printf("first loop: %d\n", *(a + i));
+		sum_main += *(a + i);
+	}
+	for (i = (size * size) - size; i > 0; i -= size - 1)
+	{
+		printf("second loop: %d\n", *(a + i));
+		sum_other += *(a + i);
+	}
+
+	printf("%d, %d\n", sum_main, sum_other);
+}
