c0c7b40ca11164f3957ad94ad8acd933c3e09022
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-09-29 01:03:41 +0300

    This is supposed to work

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,35 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * palind3 - compares string vs string reverse
+ * @a: string
+ * @l: length
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int palind3(char *a, int l)
+{
+	if (*a != *(a + l))
+		return (0);
+	else if (*a == 0)
+		return (1);
+	return (palind3(a + 1, l - 2));
+}
+
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to evaluate
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int is_palindrome(char *s)
+{
+	int l = strlen(s);
+
+	return (palind3(s, l));
+}
