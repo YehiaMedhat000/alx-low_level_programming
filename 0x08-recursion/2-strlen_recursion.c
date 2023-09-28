a46cfddd28b66727b265df4845a0f131e2f44ddb
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-09-28 13:39:32 +0300

    Added the 2-strlen_recursion.c file

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Prints a string in reverse.
+ * @s: Char pointer.
+ * Return: The length of s.
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == 0)
+	{
+		return 0;
+	}
+
+	return 1 + _strlen_recursion(s + 1);
+}
