ad3401d6ac7d7eed64862381ce338cc4814d80b6
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-10-02 19:58:13 +0300

    Edited the 4-add.c file

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,7 +14,17 @@ int main(int argc, char **argv)
 
 	if (argc > 1)
 	{
+		for (i = 1; i < argc; i++)
+		{
+			if (!(*argv[i] >= 48 && *argv[i] <= 57))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
 		total = 0;
+
 		for (i = 1; i < argc; i++)
 		{
 			total += atoi(argv[i]);
@@ -27,10 +37,5 @@ int main(int argc, char **argv)
 		printf("0\n");
 	}
 
-	else
-	{
-		printf("Error\n");
-		return (1);
-	}
 	return (0);
 }
