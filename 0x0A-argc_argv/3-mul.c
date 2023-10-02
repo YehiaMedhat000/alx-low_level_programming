8afba1adf812a42dbab63c3c929831c9a52acf96
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-10-02 19:19:17 +0300

    Added the 3-mul.c file

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: Number of arguments
+ * @argv: Array of arguments passed to the executable
+ * Return: Always success (0)
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", (*argv[1] - 48) * (*argv[2] - 48));
+	return (0);
+}
