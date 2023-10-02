630ff49b877e00a39cc7e10bddf3c47473a537d3
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-10-02 19:27:46 +0300

    Edited the 3-mul.c file

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,12 +9,16 @@
 
 int main(int argc, char **argv)
 {
+	int n1 = *argv[1] - 48;
+	int n2 = *argv[2] - 48;
+	int total = n1 * n2;
+
 	if (argc < 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	printf("%d\n", (*argv[1] - 48) * (*argv[2] - 48));
+	printf("%d\n", total);
 	return (0);
 }
