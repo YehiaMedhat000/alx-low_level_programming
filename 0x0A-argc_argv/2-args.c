7edaefc7c05e2c7cade268a9ad1ba3cf4fe863f7
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-10-02 18:27:51 +0300

    Added the 2-args.c file

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: Number of arguments
+ * @argv: Array of arguments passed to the executable
+ * Return: Always success (0)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
