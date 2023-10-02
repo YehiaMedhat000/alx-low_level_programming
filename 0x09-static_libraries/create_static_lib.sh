d018c8003fce0f399b3ff54f1d43facf21ab7443
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-10-02 15:59:56 +0300

    Edited create_...

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-# The gcc command for getting object files
 gcc -Wall -Werror -Wextra -pedantic -c *.c
-
-# Want to add conditional to handle errors or logical errors of gcc
-
-# The ar command 
 ar -rc liball.a *.o
-
-# Make the indexing
 ranlib liball.a
-
-# Add conditional to handle errors
-
-# Remove the object (.o) files
 rm *.o
