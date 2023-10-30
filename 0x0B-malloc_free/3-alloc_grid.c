71ea731ba811ed207800955598468754978226a4
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-10-30 10:53:07 +0200

    l

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -3,7 +3,6 @@
 
 /**
  * alloc_grid - create a 2d integer grid using malloc.
- * Description: Each element of the grid should be initialized to 0.
  * @width: int size width
  * @height: int size height
  * Return: Pointer to new grid
