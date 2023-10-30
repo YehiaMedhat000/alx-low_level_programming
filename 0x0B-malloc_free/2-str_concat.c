883228ef8ab7d8a175d38e5dafdf6198f7693177
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-10-30 10:46:51 +0200

    Added the 2-str_concat.c file

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -5,7 +5,7 @@
  * str_concat - concatenate two strings using malloc
  * @s1: string 1
  * @s2: string 2
- * Return: pointer to concat string
+ * Return: pointer to the concatenated string
  */
 
 char *str_concat(char *s1, char *s2)
