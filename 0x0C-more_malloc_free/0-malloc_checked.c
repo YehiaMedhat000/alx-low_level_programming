804a6726ae9dc70fd95b6ecee1a8b48bccea86fe
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-11-02 09:13:17 +0200

    Added the first task 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Allocates memory using malloc
+ * @b: Size of the block allocated
+ * Return: Pointer to the allocated memory,
+ * In case of failure returns 98
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p = malloc(b);
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return (0);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * This is a header file for the 
+ * 0x0C-More_malloc_free
+ */
+
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+
+#endif
+
