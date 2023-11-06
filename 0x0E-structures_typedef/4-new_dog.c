5948fe5b310c8b7b641adb55b89aa6697c74aba9
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-11-06 22:40:06 +0200

    Added the 4-new_dog.c file

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,26 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * new_dog - Creates a new dog object
+ * @name: String of the dog's name
+ * @age: The dog's age
+ * @owner: The dog's owner
+ * Return: Pointer to the dog object
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+		return (NULL);
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	return (d);
+
+}
