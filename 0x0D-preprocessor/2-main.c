161f7ef3595f748ba19e627c025e34d3da7c30e1
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-11-06 16:07:20 +0200

    Edited the 2-main.c file

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,16 +1,4 @@
-#include <unistd.h>
-#include <string.h>
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char *c)
-{
-	return (write(1, c, strlen(c)));
-}
+#include <stdio.h>
 
 /**
  * main - Program that prints its file name
@@ -20,6 +8,6 @@ int _putchar(char *c)
 
 int main()
 {
-	_putchar(__FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
