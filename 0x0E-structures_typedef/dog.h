0ab673a2d68d4c439851cf2c8376c69906cd8f34
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-11-06 21:06:00 +0200

    Edited the dog.h file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - Data type that defines dog objects
+ * @name: The dog's name
+ * @owner: The dog's owner's name
+ * @age: The dog's age
+ *
+ * Describtion: Data type for defining dog objects which 
+ * could be used in datasets later and building them 
+ * efficiently
+ */
+
 struct dog
 {
 	char *name;
