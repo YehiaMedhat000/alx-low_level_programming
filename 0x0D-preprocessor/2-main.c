6456f77a758ef1d4a6be4be7389de90cad10deb8
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-11-06 13:17:51 +0200

    Added the 2-main.c file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Program that prints its file name
+ *
+ * Return: 0 (always success)
+ */
+
+int main()
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
