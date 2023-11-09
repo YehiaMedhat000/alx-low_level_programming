998219899b8e0b05be988429b55294145bc1bdb7
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-11-09 08:07:21 +0200

    This is bad

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,62 +1,60 @@
 #include "3-calc.h"
-#include <stdlib.h>
 
 /**
- * op_add - Adds two integers
- * @a: The first integer
- * @b: The second integer
- * Return: The sum of the two integers
+ * op_add - returns sum of two numbers
+ * @a: int parameter
+ * @b: int parameter
+ *
+ * Return: (@a + @b)
  */
-
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
 /**
- * op_sub - Subtracts two numbers
- * @a: The first number
- * @b: The second number
- * Return: The difference of a and b
+ * op_sub - returns difference of two numbers
+ * @a: int parameter
+ * @b: int parameter
+ *
+ * Return: (@a - @b)
  */
-
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
 /**
- * op_mul - Multiplies two numbers
- * @a: The first number
- * @b: The second number
- * Return: The multiplication
+ * op_mul - returns product of two numbers
+ * @a: int parameter
+ * @b: int parameter
+ *
+ * Return: (@a * @b)
  */
-
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
 /**
- * op_div - Divides two numbers
- * @a: The first number
- * @b: The second number
- * Return: The division of a over b
+ * op_div - returns quotient of two numbers
+ * @a: int parameter
+ * @b: int parameter
+ *
+ * Return: (@a / @b)
  */
-
 int op_div(int a, int b)
 {
 	return (a / b);
 }
 
 /**
- * op_mod - Get the remaider
- * of the division of a and b
- * @a: The first number
- * @b: The second number
- * Return: The remainder of a over b
+ * op_mod - returns the remainder from division of two numbers
+ * @a: int parameter
+ * @b: int parameter
+ *
+ * Return: (@a % @b)
  */
-
 int op_mod(int a, int b)
 {
 	return (a % b);
