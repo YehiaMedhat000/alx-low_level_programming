f7171550567a6ddc43b35ba640ffd7806b5a12bb
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-11-09 21:51:47 +0200

    Deleted whitespace

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -16,12 +16,11 @@ int sum_them_all(const unsigned int n, ...)
 		return (0);
 
 	va_start(params, n);
-	
+
 	for (i = 0; (unsigned int)i < n; i++)
 		sum += va_arg(params, int);
 
 	va_end(params);
 
 	return (sum);
-
 }
