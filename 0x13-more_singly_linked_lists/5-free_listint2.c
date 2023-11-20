6056ee7730fbd36a7af7f49d630f2d210d4ce0f7
YehiaMedhat <avhmedmvmedhat000@gmail.com>
2023-11-20 18:08:22 +0200

    Edited the 5-free_listint2.c file

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,14 +9,13 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *temp, *current_node;
+	listint_t *temp;
 
-	current_node = *head;
-	while (current_node)
+	while (*head)
 	{
-		temp = current_node;
-		current_node = current_node->next;
-		free(temp);
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
 	}
 	*head = NULL;
 }
