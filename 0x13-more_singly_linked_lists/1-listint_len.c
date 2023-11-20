ac7d51b16fdb8f0efe16f366b4b17168f929abeb
YehiaMedhat <avhmedmvmedhat000@gmail.com>
2023-11-20 16:08:48 +0200

    Added the first 4 tasks

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -6,7 +6,7 @@
  * Return: The number of elements
  */
 
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	size_t len = 0;
 	while (h)
---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - Adds a new node
+ * to the end of the list
+ * @head: Pointer to the list of nodes
+ * @n: The integer to put in the node
+ * Return: Pointer to the new node
+ * NULL in case of failure
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new_node = (listint_t *)malloc(sizeof(listint_t));
+	listint_t *node = (listint_t *)malloc(sizeof(listint_t));
+
+	if (!new_node || !head || !node)
+		return (NULL);
+
+	new_node->next = NULL;
+	new_node->n = n;
+	/* In case the list has no nodes */
+	if (!*head)
+		*head = new_node;
+	else
+	{
+		node = *head;
+		/* Get to the end of list */
+		while (node)
+			node = node->next;
+		node->next = new_node;
+	}
+
+	return (new_node);
+}
+
