4e8f363e507e5f9e7e75429156a7f21199326ef1
YehiaMedhat <avhmedmvmedhat000@gmail.com>
2023-11-20 18:01:20 +0200

    Added another solution for the f****** test cases

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,33 +1,34 @@
 #include "lists.h"
 
 /**
- * add_nodeint_end - Adds a new node
- * to the end of the list
- * @head: Pointer to the list of nodes
- * @n: The integer to put in the node
- * Return: Pointer to the new node
- * NULL in case of failure
+ * add_nodeint_end - adds a node at the end of a linked list
+ * @head: pointer to the first element in the list
+ * @n: data to insert in the new element
+ *
+ * Return: pointer to the new node, or NULL if it fails
  */
-
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *node = malloc(sizeof(listint_t));
+	listint_t *new;
+	listint_t *temp = *head;
 
-	if (!node || head)
-		return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (!new)
+	return (NULL);
 
-	node->n = n;
-	node->next = NULL;
+	new->n = n;
+	new->next = NULL;
 
-	if (!*head)
+	if (*head == NULL)
 	{
-		*head = node;
-		return (node);
+	*head = new;
+	return (new);
 	}
-	while (*head)
-		(*head) = (*head)->next;
 
-	(*head)->next = node;
-	return (node);
+	while (temp->next)
+	temp = temp->next;
+
+	temp->next = new;
+
+	return (new);
 }
-
