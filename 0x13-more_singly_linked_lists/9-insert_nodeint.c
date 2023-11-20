b3dd139c93b0c8a0ee64e2ed3b57012dc2769335
YehiaMedhat <avhmedmvmedhat000@gmail.com>
2023-11-20 20:33:15 +0200

    Edited the solution for the 9-insert_nodeint.c file

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,26 +12,35 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_node = malloc(sizeof(listint_t));
-	listint_t *temp, *cursor;
+	listint_t *temp = *head;
 
 	new_node->n = n;
-	cursor = *head;
+	new_node->next = NULL;
+
 	/* Safety checks */
-	if (!new_node || !head || !cursor)
+	if (!new_node || !head)
 		return (NULL);
 
-	/* Get to before idx */
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+
+	/* Get to idx */
 	idx -= 1;
-	while (idx--)
-		cursor = cursor->next;
+	while (idx-- && temp)
+	{
+		temp = temp->next;
 
-	/* Assign values */
-	temp = cursor->next;
-	cursor->next = new_node;
-	new_node->next = temp;
-	/* Check for failures */
-	if (new_node || temp || cursor)
-		return (NULL);
+		if (idx == 0)
+		{
+			new_node->next = temp->next;
+			temp->next = new_node;
+			return (new_node);
+		}
+	}
 
-	return (new_node);
+	return (NULL);
 }
