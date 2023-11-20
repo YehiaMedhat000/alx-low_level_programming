4e2af2dca61665081deaf20c17ead16062c62449
YehiaMedhat <avhmedmvmedhat000@gmail.com>
2023-11-20 18:24:50 +0200

    Added the 8-sum_listint.c file

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -4,4 +4,22 @@
  * sum_listint - Sums all values in a
  * linked list
  * @head: Pointer to the head node
- * 
+ * Return: Summation of all integers
+ * in the linked list
+ * (0) if the list is empty
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (!head)
+		return(0);
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
