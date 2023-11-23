1b3b93b60db4a2a0672570f2b321485a564f3c55
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-11-23 20:14:10 +0200

    Fixing betty

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * set_bit - Sets the bit in the ith index to 1
+ * set_bit - Sets the bit in the
+ * ith index to 1
  * @n: Pointer to the number
  * @index: The index of the needed bit
  * Return: 1 if success, -1 otherwise
