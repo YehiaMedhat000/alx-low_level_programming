dea92851857732f0166bda9ac59a3f243d91136c
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-11-23 20:11:40 +0200

    Added the 3-set_bit.c file

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * set_bit - Sets the bit in the ith index to 1
+ * @n: Pointer to the number
+ * @index: The index of the needed bit
+ * Return: 1 if success, -1 otherwise
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(*n) * 8)
+		return (-1);
+	
+	return (!!(*n |= 1L << index));
+}
