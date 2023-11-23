5fb7560fe64da668f07b9a469e5e5a16b7f7b71e
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-11-23 20:22:05 +0200

    Edited the 4-clear_bit.c file

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,7 +8,7 @@
  * Return: 1 if success, -1 otherwise
  */
 
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= sizeof(*n) * 8)
 		return (-1);
