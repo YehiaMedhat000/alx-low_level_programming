20496428ec115eb307b91c9395764123f9125c37
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-11-27 20:48:04 +0200

    Edited the 2-append_text_to_file.c file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,7 +9,7 @@
  * Return: (1) success, (-1) failure
  */
 
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
 	ssize_t bytes = 0, len = strlen(text_content);
