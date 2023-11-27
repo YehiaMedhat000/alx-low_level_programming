d0f20933f21eb3439cef4788ab3f94893158c64a
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-11-27 20:41:06 +0200

    Added the 1-create_file.c file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file
+ * @filename: File name
+ * @text_content: Contents to put in
+ * the file
+ * Return: (1) success, (-1) failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes = 0, len = strlen(text_content);
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+	if (len)
+		bytes = write(fd, text_content, len);
+	close(fd);
+	return (bytes == len ? 1 : -1);
+}
