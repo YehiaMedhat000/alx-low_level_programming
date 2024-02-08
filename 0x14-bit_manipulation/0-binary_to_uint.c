a0c53c4f0d68838427d9f9bf3915e80f04b382d3
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2024-02-08 20:41:09 +0200

    0-binary_to_uint.c: Made my own solution

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,15 +11,15 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num = 0;
+	int len = strlen(b) - 1;
 
 	if (!b)
 		return (0);
-
 	while (*b)
 	{
 		if (*b != '0' && *b != '1')
 			return (0);
-		num = num * 2 + (*b++ - '0');
+		num += (*b++ - 48) << len--;
 	}
 	return (num);
 }
