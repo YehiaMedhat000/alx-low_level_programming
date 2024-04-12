0b1907338c6df2bf563e30eb6cb13ac8d7591e57
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2024-04-13 00:29:08 +0200

    Added the third task

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,13 +1,50 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - 
- *
- * Return: 
+ * hash_table_set - Adds an element to the table
+ * @ht: The hast table to add to
+ * @key: The key value of the element
+ * @value: The numeric value of the element
+ * Return: 1 in success, 0 otherwise
  */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	hash_node_t *node;
+	unsigned long int idx, j;
 
-	return ();
+	if (!key || !ht || !*key || !value)
+		return (0);
+
+	cpy = strdup(value);
+	if (!cpy)
+		return (0);
+
+	idx = key_index((const unsigned char *)key, ht->size);
+	for (j = 0; ht->array[j]; j++)
+	{
+		if (strcmp(ht->array[j]->key, key) == 0)
+		{
+			free(ht->array[j]->value);
+			ht->array[j]->value = cpy;
+			return (1);
+		}
+	}
+
+	node = malloc(sizeof(hash_node_t));
+	if (!node)
+	{
+		free(cpy);
+		return (0);
+	}
+	node->key = strdup(key);
+	if (!node->key)
+	{
+		free(node);
+		return (0);
+	}
+	node->value = cpy;
+	node->next = ht->array[idx];
+	ht->array[idx] = node;
+	return (1);
 }
