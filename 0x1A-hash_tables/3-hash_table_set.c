55c91096fa2c4feb093a1ee461b10de1d88b4ef9
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2024-04-13 00:31:04 +0200

    Fixed some issue

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,6 +12,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *node;
 	unsigned long int idx, j;
+	char *cpy;
 
 	if (!key || !ht || !*key || !value)
 		return (0);
