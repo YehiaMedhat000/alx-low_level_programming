d9498504f5525e28d5d534b237cb173fa61c147e
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2024-04-12 14:15:29 +0200

    0-hash_table_create.c: The 1st task

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hash_table
+ * in the memory by malloc
+ * @size: The size of the table to allocate
+ * Return: Pointer to the hash_table allocated
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table = malloc(sizeof(hash_table_t));
+
+	if (!table)
+		return (NULL);
+
+	table->size = size;
+	table->array =  malloc(sizeof(hash_node_t *) * size);
+
+	if (!table->array)
+		return (NULL);
+
+	while (--size)
+		table->array[size] = NULL;
+
+	return (table);
+}
