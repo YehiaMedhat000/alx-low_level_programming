f7c46f2926b38abfde7bf950922a364f992d21ef
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2024-04-12 23:48:23 +0200

    HHHHHHHHHHHHHHHH

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -3,19 +3,18 @@
 /**
  * hash_djb2 - implementation of the djb2 algorithm
  * @str: string used to generate hash value
- *
  * Return: hash value
  */
 
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
-	int c;
 
 	hash = 5381;
-	while ((c = *str++))
+	while (*str)
 	{
-		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+		hash = ((hash << 5) + hash) + *str; /* hash * 33 + c */
+		str++;
 	}
 	return (hash);
 }
