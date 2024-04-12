9fa3450740849204f72e6fbdd3b6b56abafd8057
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2024-04-12 23:55:31 +0200

    0-hash_table_create.c: Fixed somethings

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,11 +17,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table->size = size;
 	table->array =  malloc(sizeof(hash_node_t *) * size);
 
-	if (!table->array)
+	if (table->array == NULL)
+	{
+		free(table);
 		return (NULL);
-
-	while (--size)
-		table->array[size] = NULL;
+	}
+	while (size)
+		table->array[size--] = NULL;
 
 	return (table);
 }
