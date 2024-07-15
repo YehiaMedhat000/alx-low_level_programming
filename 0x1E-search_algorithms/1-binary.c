22a5e40077392f701952694ca207feac01403d6c
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2024-07-16 00:36:37 +0300

    Added the 2nd task completed and tested

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,6 +1,29 @@
 #include "search_algos.h"
 
 /**
+ * print_array - Utility to print an array
+ * @array: The array to print
+ * @left: The lower index of the array
+ * @right: The upper index of the array
+ * Return: Nothing (void)
+ */
+
+void print_array(int *array, size_t left, size_t right)
+{
+	size_t i;
+
+	printf("Searching in array: ");
+
+	for (i = left; i <= right; i++)
+	{
+		if (i > left)
+			printf(", ");
+		printf("%d", array[i]);
+	}
+	printf("\n");
+}
+
+/**
  * binary_search - Function that implements the
  * binary_search algorithm
  * @array: The array to search
@@ -13,9 +36,23 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	size_t low, mid;
+	size_t left = 0, mid;
+	size_t right = size - 1;
 
-	if (!array || !size)
+	if (array == NULL || size == 0)
 		return (-1);
 
+	while (left <= right)
+	{
+		print_array(array, left, right);
+		mid = left + (right - left) / 2;
+
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
+	return (-1);
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,8 +3,20 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
+/* Print arrays */
+void print_array(int *array, size_t left, size_t right);
+
+/* Main tasks */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+
+/* Advanced tasks */
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+
 
 #endif /* _SEARCH_ALGO_ */
