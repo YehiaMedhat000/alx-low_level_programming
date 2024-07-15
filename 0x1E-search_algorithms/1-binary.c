487d5faebd5af254778c11cde1be6b0d15267eb3
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2024-07-15 21:23:01 +0300

    Added the 3rd task

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,21 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - Function that implements the
+ * binary_search algorithm
+ * @array: The array to search
+ * @size: The size of the array
+ * @value: The value to search
+ * Return: The first index where it finds value
+ * if value isn't found, returns -1
+ * or if array is NULL
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t idx;
+
+	if (!array || !size)
+		return (-1);
+
+}
