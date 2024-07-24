a03687f12f7ef87f14272d0fcd315e9d1f255a0b
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2024-07-24 22:50:52 +0300

    Added the final edits to the code

---
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -0,0 +1,15 @@
+#include "search_algos.h"
+
+/**
+ * linear_skip - Function that searches a linked list for value
+ * using the linear search algo with skipping sublists
+ * @list: The list to search in
+ * @value: The value to search for
+ * Return: Pointer to the node where the value is found
+ * Otherwise, NULL
+ */
+
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+
+}
